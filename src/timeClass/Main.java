f5579c33379eccf9c4c47e081efde636ebdb4f01
Dwan72 <alex.cap.an@gmail.com>
2021-10-18 20:37:13 -0500

    "first commit"

---
+++ src/timeClass/Main.java
@@ -0,0 +1,11 @@
+package timeClass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ src/timeClass/State.java
@@ -0,0 +1,35 @@
+package timeClass;
+
+public class State {
+    private char c;
+    private int sum;
+    private int total;
+
+
+
+
+
+    public void setC(char c) {
+        this.c = c;
+    }
+    public char getC() {
+        return this.c;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+    public int getSum() {
+        return this.sum;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    public int getTotal() {
+        return this.total;
+    }
+
+
+
+}
